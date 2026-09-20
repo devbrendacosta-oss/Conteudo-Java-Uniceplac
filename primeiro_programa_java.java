2900413ca436cdd80f0a9dae596d9c361d7e958c
Brenda Costa <devbrendacosta@gmail.com>
2026-09-20 19:50:00 -0300

    Add files via upload

---
+++ primeiro_programa_java.java
@@ -0,0 +1,22 @@
+// AULA 4 GUSTAVO GUANABARA - PRIMEIRO PROGRAMA EM JAVA: https://youtu.be/ijXXaIYsVx4?si=8rmiJ5oxkt8RmHL7
+
+package primeirademonstracao; // PACOTE = PASTA ONDE FICAM ARMAZENADOS OS ARQUIVOS
+
+public class PrimeiroProgramaJava { // TODO PROGRAMA PRECISA DE UMA CLASSE
+
+	public static void main(String[] args) { // MÉTODO PRINCIPAL "MAIN" - PONTO DE PARTIDA DE UM PROGRAMA 
+		
+		System.out.print("Olá, Mundo!"); // COMANDO DE SAÍDA DE DADOS = PARA EXIBIR MENSAGEM NA TELA - TAMBÉM EXISTE O .println, QUE ESCREVE E PULA PARA LINHA DE BAIXO
+
+        /* 
+         * ==========================================
+         * CAMELCASE: REGRAS DE CASE SENSITIVE
+         * ==========================================
+         * INTERFACE OU CLASSE: PRIMEIRA LETRA MAIÚSCULA, RESTANTE EM CAMELCASE (EXEMPLO: NomeAluno)
+         * VARIÁVEIS, ATRIBUTOS E MÉTODOS: PRIMEIRA LETRA MINÚSCULA, RESTANTE EM CAMELCASE (EXEMPLO: nomeAluno)
+         * PACOTES: TODAS LETRAS MINÚSCULAS (EXEMPLO: primeirademonstracao)
+         * CONSTANTE: TODAS LETRAS MAIÚSCULAS, SEPARADAS POR UNDERLINE "_" (EXEMPLO: VALOR_DE_PI)
+         */
+		
+	}
+}
